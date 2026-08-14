5ad98125cc15db3f41e8f6c0871e98a15f37944c
Yana <averyanusha@gmail.com>
2026-08-14 13:38:56 +0100

    Finished responsive design

--- src/components/pages/Faq.tsx
+++ src/components/pages/Faq.tsx
@@ -4,13 +4,13 @@ export default function Faq(){
     <div className='container'>
       <h1 className="title">Game Rules</h1>
       <p className='subtitle'>Test your memory</p>
-      <ul className='rules'>
+      <ol className='rules'>
         <li className='rules-item'>Choose the level of difficulty</li>
         <li className='rules-item'>Choose one of the cards, memorize it, click on it</li>
         <li className='rules-item'>After the cards are shuffled click on the other card</li>
         <li className='rules-item'>Your goal is not to click on the same card twice</li>
         <li className='rules-item'>For every won game you gain 100xp and for every time you stayed in the game more than 3 rounds you gain 20xp</li>
-      </ul>
+      </ol>
     </div>
     </>
   )
